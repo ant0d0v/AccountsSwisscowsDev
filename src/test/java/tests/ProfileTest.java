888cbfa3f0f48db58298b8cb0a4c318b7954badd
ant0d0v <gosu1203@gmail.com>
2023-04-18 18:11:23 +0300

    ProfilePage

--- src/test/java/tests/ProfileTest.java
+++ src/test/java/tests/ProfileTest.java
@@ -176,7 +176,7 @@ public class ProfileTest extends BaseTest {
 
     }
 
-    @Test(dataProvider = "LangProfilePageTestData", dataProviderClass = TestData.class)
+    @Test(priority = 8,dataProvider = "LangProfilePageTestData", dataProviderClass = TestData.class)
     public void testChangeLocalisationForExternalUser_ProfilePage(
             int index, String expectedH1Text) throws InterruptedException {
         ProfilePage profilePage = new  ProfilePage(getDriver());
@@ -197,7 +197,7 @@ public class ProfileTest extends BaseTest {
         Assert.assertNotEquals( oldH1text, actualH1text);
         Assert.assertEquals( actualH1text, expectedH1Text);
     }
-    @Test( dataProvider = "LangProfilePageTestData", dataProviderClass = TestData.class)
+    @Test(priority = 9,dataProvider = "LangProfilePageTestData", dataProviderClass = TestData.class)
     public void testChangeLocalisationForSwisscowsUser_ProfilePage(
             int index, String expectedH1Text) throws InterruptedException {
         ProfilePage profilePage = new  ProfilePage(getDriver());
