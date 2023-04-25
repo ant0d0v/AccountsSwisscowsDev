9b0a6ef7efdb77eab1b8fa2b7d2150682bfd5708
ant0d0v <gosu1203@gmail.com>
2023-04-25 18:06:26 +0300

    refractor testLoginToSiteSwisscowsEmailForSwisscowsUser

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -141,7 +141,7 @@ public class LoginTest extends BaseTest {
                 .openSwisscowsEmailForm()
                 .waitMainImageToBeVisible_LoginPage()
                 .enterNewUserEmail(ProjectConstants.SWISSCOWS_EMAIL_USER)
-                .enterNewUserPassword(ProjectConstants.PASSWORD)
+                .enterNewUserPassword(ProjectConstants.NEW_PASSWORD)
                 .clickLoginButton()
                 .waitUtilToBeVisibleSwisscowsEmailBoxLogo()
                 .getTitle();
