b8ac9d9fdd17d3436b716c92ee416a76df738c0b
Anton Udovychenko <97831314+ant0d0v@users.noreply.github.com>
2023-05-11 11:36:38 +0300

    refractor test of email platinum subscription (#52)

--- src/test/java/pages/sidebar_menu/PlatinumPage.java
+++ src/test/java/pages/sidebar_menu/PlatinumPage.java
@@ -1,16 +1,33 @@
 package pages.sidebar_menu;
 
+import io.qase.api.annotation.Step;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import pages.base_abstract.SidebarMenuPage;
 
+import java.util.List;
+
 public class PlatinumPage extends SidebarMenuPage<PlatinumPage> {
     @FindBy(xpath = "//a[@class='btn-submit']")
     private WebElement buyNowButtonOfProduct;
+    @FindBy(xpath = "//div[@class ='description']//a")
+    private List<WebElement> linkOfPlatinumPage;
     public PlatinumPage(WebDriver driver) {
 
         super(driver);
+    }
+    @Step("Click link of platinum page")
+    public void clickLinksOfPage(int index) {
+        click(getAllLinksOnPage().get(index));
+        if (getDriver().getWindowHandles().size() > 1) {
+            switchToAnotherWindow();
+        }
+        createGeneric();
+    }
+    public List<WebElement> getAllLinksOnPage() {
+
+        return  linkOfPlatinumPage;
     }
 
     public PlatinumPage createGeneric() {
