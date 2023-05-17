b04c794ab3c8aaefff0656f2f5ecbc0b6d0060b5
ant0d0v <gosu1203@gmail.com>
2023-05-17 17:26:03 +0300

    refractor tests

--- src/test/java/pages/base_abstract/BasePage.java
+++ src/test/java/pages/base_abstract/BasePage.java
@@ -537,7 +537,7 @@ public abstract class BasePage {
     }
     @Step("Get count of message on the gmail box")
     public int getMessageCountToGmailBox() throws MessagingException, IOException, InterruptedException {
-        waitSeveralSeconds(10);
+        waitSeveralSeconds(12);
         EmailUtils.PropertiesGmail propertiesEmail = new EmailUtils.PropertiesGmail();
         Properties props = propertiesEmail.setServerProperties();
 
@@ -570,7 +570,7 @@ public abstract class BasePage {
         int messageCount = inbox.getMessageCount();
 
         while (true) {
-            waitSeveralSeconds(5);
+            waitSeveralSeconds(8);
 
             inbox = store.getFolder("inbox");
             inbox.open(Folder.READ_WRITE);
